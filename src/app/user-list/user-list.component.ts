b0af2d5bee675bd0edde78dc4539522b61329cff
pchandv <pravinchandvisam@gmail.com>
2018-08-31 12:45:12 +0530

    FromVs

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class UserListComponent implements OnInit {
   names: string[];
   constructor() {
-    this.names = ['Lalliy', 'Praveen', 'Druthi sree', 'Visam', 'Lokesh'];
+    this.names = ['Lalliy', 'Praveen', 'Druthi sree', 'Visam', 'Lokesh', 'Wari'];
   }
 
   ngOnInit() {
